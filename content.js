c2435d19605a7e6f51cd5d7f0aac3f3754a9b399
utkarshtiwari2904-crypto <utkarshtiwari2904@gmail.com>
2026-07-02 22:02:54 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,25 @@
-{
-title:"Every great story has a first page.",
-message:"Mine began at a dissection table."
-}
+const scenes = [
+  {
+    title: "",
+    message: "",
+    button: false
+  },
+
+  {
+    title: "Every great story has a first page.",
+    message: "",
+    button: false
+  },
+
+  {
+    title: "",
+    message: "Mine began at a dissection table.",
+    button: false
+  },
+
+  {
+    title: "",
+    message: "",
+    button: true
+  }
+];
